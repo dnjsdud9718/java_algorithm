02a4ac59c4dda09f98f7e3ff178a1b54a5aa0030
wonyoung <dnjsdud9718@naver.com>
2024-01-05 22:22:52 +0900

    240105

---
+++ src/Q6/T48/Main.java
@@ -0,0 +1,75 @@
+package Q6.T48;
+// 물의 양 구하기 2251 백준 그래프 문제 아이디어 떠올리기 쉽지 않네 코드 짜기 쉽지 않네....~!!!!!!!!!
+/*
+ * 각각의 부피가 A,B,C(1<= A,B,C<=200) 리터인 3개의 물통이 있다. 처음에는 앞의 두 물통은 비어 있고, 3번째 물통은 
+ * 가득(C리터)차 있다. 이제 어떤 물통에 들어 있는 물을 다른 물통으로 쏟아부을 수 있는데, 이때는 한 물통이 비거나,
+ * 다른 한 물통이 가득찰 때까지 물을 부을 수 있다. 이 과정에서 손실되는 물은 없다고 가정한다. 이와 같은 과정을 거치다
+ * 보면 3번째 물통(용량이 C인)에 담겨 있는 물의 양이 변할 수도 있다. 1번째 물통(용량이 A인)이 비어 있을 때 3번째(용량이 C인)에
+ * 담겨 있을 수 있는 물의 양을 모두 구하는 프로그램을 작성하시오.
+ */
+import java.util.*;
+public class Main {
+    static int[] sender = {0,0,1,1,2,2}; // 보내는 물통
+    static int[] receiver = {1,2,0,2,0,1}; // 받는 물통
+    static boolean[][] visited;
+    static boolean[] answer;
+    static int[] volume; // 물통의 최대량
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        volume = new int[3]; // A:0 B:1 C:2
+        volume[0] = sc.nextInt(); // A가 담을 수 있는 최대 부피
+        volume[1] = sc.nextInt(); // B가 담을 수 있는 최대 부피
+        volume[2] = sc.nextInt(); // C가 담을 수 있는 최대 부피
+        sc.close();
+
+        visited = new boolean[201][201]; // A와 B 물통이 담은 물의 양, C 물통의 양은 자연히 구할 수 있다.
+        answer = new boolean[201];
+        bfs();
+
+        for(int i=1; i<=200; i++) if(answer[i]) System.out.printf("%d ", i);
+    }
+    
+    public static void bfs(){
+        visited[0][0] = true;
+        answer[volume[2]] = true;
+        Queue<AB> queue = new LinkedList<>();
+        queue.add(new AB(0,0)); //A,B 양을 안다면 C도 자동으로 알 수 있다.
+        
+        while(!queue.isEmpty()){
+            AB current = queue.poll();
+            int A = current.A;
+            int B = current.B;
+            int C = volume[2] - A - B;
+            
+            for(int i=0; i<6; i++){ //A->B, A->C, B->A, B->C, C->A, C->B 6가지 경우가 있다.
+                int[] next = {A,B,C};    
+                next[receiver[i]] += next[sender[i]]; //일단 경우에 따라 물을 붓는다고 가정.
+                next[sender[i]] = 0;
+
+                //초과된 경우. 즉, 물이 넘친 경우
+                if(next[receiver[i]] > volume[receiver[i]]){
+                    next[sender[i]] = next[receiver[i]] - volume[receiver[i]];
+                    next[receiver[i]] = volume[receiver[i]];
+                }
+                //방문 여부 확인
+                if(!visited[next[0]][next[1]]){
+                    visited[next[0]][next[1]] = true;
+                    queue.add(new AB(next[0], next[1]));
+                    if(next[0] == 0) answer[next[2]] = true;
+                }
+            }
+        }
+
+    }
+    
+}
+
+class AB{
+    int A;
+    int B;
+    public AB(int A, int B){
+        this.A = A;
+        this.B = B;
+    }
+}
---
+++ src/swea/Q1/T1/Solution.java
@@ -0,0 +1,73 @@
+package swea.Q1.T1;
+/* 
+ * Ssafy, 최적 경로 문제
+ * 삼성전자의 서비스 기사인 김기사는 회사에서 출발하여 냉장고 배달을 위해 N명의 고객을 방문하고 자신의 집에 돌아가려한다.
+ * 회사와 집의 위치, 그리고 각 고객의 위친는 이차원 정수 좌표(x,y)로 주어지고(0<=x<=100, 0<=y<=100)
+ * 두 위치 (x1,y1)와 (x2,y2) 사이의 거리는 |x1-x2|+|y1-y2|으로 계산된다.
+ * 회사의 좌표, 집의 좌표, 고객들의 좌표는 모두 다르다.
+ * 회사에서 출발하여 N명의 고객을 모두 방문하고 집으로 돌아오는 경로 중 가장 짧은 것을 찾으려 한다.
+ * 회사와 집의 좌표가 주어지고, 2명에서 10명 사이의 고객 좌표가 주어질 때,
+ * 회사에서 출발해서 이들을 모두 방문하고 집에 돌아가는 경로 중 총 이동거리가 가장 짧은 경로를 찾는 프로그램을 작성하라.
+ * 
+ * 이 문제는 가장 짧은 경로를 '효율적으로' 찾는 것이 목적이 아니다.
+ * 여러분은 모든 경로를 살펴서 해를 찾아도 좋다.
+ * 모든 경우를 체계적으로 따질 수 있으면 정답을 맞출 수 있다.
+ * 
+ * ★ DFS 특정 노드에서 백트래킹 해당 노드를 방문하지 않음으로 초기화하고 나가야 한다.
+ * 
+ * A B C 고객 집이 있다면
+ * 모든 가능한 경로 설정해 본다
+ * 3! = 6가지
+ * A B C
+ * A C B
+ * B A C
+ * B C A
+ * C A B
+ * C B A
+ */
+import java.util.stream.Stream;
+import java.io.*;
+public class Solution {
+    static int[] iArr; //입력 배열
+    static boolean[] visited; // 방문 배열
+    static int N; //입력 크기
+    static int sum; 
+    static int count; //DFS 깊이
+    static int min; // 최솟값
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int ts=1; ts<=T; ts++){
+            sum  = 0;
+            min  = Integer.MAX_VALUE;
+            N = Integer.parseInt(br.readLine());
+            iArr = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            visited = new boolean[N+2];
+            count = -1;
+            dfs(iArr[0], iArr[1], 0);
+            System.out.printf("#%d %d\n", ts, min);
+        }
+        br.close();
+    }
+    public static void dfs(int x, int y, int idx){
+        visited[idx] = true;
+        count++;
+        //System.out.printf("%d ", idx);
+        if(count == N) { // DFS가 최대 깊이 도달했을 때, 즉, 모든 노드를 방문한 경우, 집까지의 거리 계산
+            int distance = Math.abs(x - iArr[2]) + Math.abs(y - iArr[3]);
+            sum += distance;
+            if(min > sum) min = sum;
+            sum -= distance;
+        }
+        for(int i=2; i<N+2; i++){ // 탐색
+            if(!visited[i] && idx != i){ // 자기자신이 아니고 방문하지 않은 경우
+                int tmp = Math.abs(x-iArr[i*2]) + Math.abs(y-iArr[i*2+1]); //거리 계산
+                sum += tmp;
+                dfs(iArr[i*2], iArr[i*2+1], i);
+                sum -= tmp;
+            }
+        }
+        visited[idx] = false;
+        count--;
+    }
+}

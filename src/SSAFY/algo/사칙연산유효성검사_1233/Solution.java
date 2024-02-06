7e0d9640d4d3e2ce1d0af167213fe2509dd50463
장지원 <wldnjs980409@naver.com>
2024-02-06 17:43:06 +0900

    commit test11

--- src/SSAFY/algo/사칙연산유효성검사_1233/Solution.java
+++ src/SSAFY/algo/사칙연산유효성검사_1233/Solution.java
@@ -3,7 +3,7 @@ package SSAFY.algo.사칙연산유효성검사_1233;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
 
 // 노드 유효성 검사
 // 노드가 숫자이면 -> 자식이 없어야 한다.
@@ -26,21 +26,32 @@ public class Solution {
                 sb.append("#").append(t).append(" ").append(0).append("\n");
                 continue;
             }
-            ans = 1;
-            int idx = N; // 마지막 노드부터 유효성 검사
-
-            while(idx != 1){
-                //자식 노드가 모두 숫자 이고, 부모노드가 연산자이어야 함.
-                if(!Character.isDigit(node[idx]) ||  !Character.isDigit(node[idx-1]) || Character.isDigit(node[idx/2])){
-                    ans = 0;
-                    break;
-                }
-                node[idx/2] = '1'; // 부모 노드를 임의의 숫자로 처리
-                idx -= 2;
-            }
+//            ans = 1;
+//            int idx = N; // 마지막 노드부터 유효성 검사
+//
+//            while(idx != 1){
+//                //자식 노드가 모두 숫자 이고, 부모노드가 연산자이어야 함.
+//                if(!Character.isDigit(node[idx]) ||  !Character.isDigit(node[idx-1]) || Character.isDigit(node[idx/2])){
+//                    ans = 0;
+//                    break;
+//                }
+//                node[idx/2] = '1'; // 부모 노드를 임의의 숫자로 처리
+//                idx -= 2;
+//            }
+//            sb.append("#").append(t).append(" ").append(ans).append("\n");
+            ans = dfs(1) ? 1 : 0;
             sb.append("#").append(t).append(" ").append(ans).append("\n");
         }
         System.out.println(sb.toString());
         br.close();
+    }
+    public static boolean dfs(int x){
+        if(x > N) return false;
+        if(Character.isDigit(node[x])){
+            if(x*2> N) return true;
+            return false;
+        }else{
+            return dfs(x*2) && dfs(x*2+1);
+        }
     }
 }

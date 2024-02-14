d4c2f1b1379df4b1b0ef690e5049aa3fa5f28d3a
wonyoung <dnjsdud9718@naver.com>
2024-02-14 21:39:01 +0900

    0214 divide&conquer

--- src/SSAFY/algo/baekjoon/t1992/Main.java
+++ src/SSAFY/algo/baekjoon/t1992/Main.java
@@ -5,53 +5,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.stream.Stream;
-
+/*
+    백준 1992 쿼드 트리 -> 전형적인 재귀 문제
+    8
+    11110000
+    11110000
+    00011100
+    00011100
+    11110000
+    11110000
+    11110011
+    11110011
+ */
 public class Main {
-    static int N;
-    static String answer;
+    static  int N;
     static int[][] src;
+    static String answer;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        src = new int[N][];
+        src = new int[N][N];
         for (int i = 0; i < N; i++) {
             src[i] = Stream.of(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
 //            System.out.println(Arrays.toString(src[i]));
         }
-        answer = "";
-        logic(0, 0, N, N);
-        System.out.println(answer);
-
+        logic(0, 0, N);
+        System.out.println(sb.toString());
         br.close();
     }
 
-    public static void logic(int sy, int sx, int ey, int ex) {
-        if (sy == ey && sx == ex) {
-            return;
-        }
-        answer += "(";
-        int v = src[sy][sx];
-        boolean b = true;
-        for (int i = sy; i < ey; i++) {
-            for (int j = sx; j < ex; j++) {
+    static void logic(int y, int x, int n) {
+        if(n<1) return;
+//        System.out.println(y + " " + x + " " + (y+n) + " " + (x+n)+" : " + n);
+        int v = src[y][x];
+        for (int i = y; i < y + n; i++) {
+            for (int j = x; j < x + n; j++) {
                 if (v != src[i][j]) {
-                    b = false;
+                    v = -1;
                     break;
                 }
             }
-            if(!b) break;
+            if( v < 0) break;
         }
-        if (!b) {
-            System.out.println(" "+sy+" "+ sx+" "+ (ey/2) +" "+ (ex/2));
-            logic(sy, sx, ey/2, ex/2);
-            System.out.println(" "+sy+" "+ (ex/2)+" "+ (ey/2) +" "+ ex);
-            logic(sy, ex / 2, ey / 2, ex);
-            System.out.println(" "+(ey/2)+" "+sx+" "+ey+" "+(ex/2));
-            logic(ey / 2, sx, ey, ex / 2);
-            System.out.println(" "+(ey/2)+" "+(ex/2) + " "+ ey+" "+ ex);
-            logic(ey/2, ex/2, ey, ex);
-        } else {
-            answer += v + ")";
+        if(v >= 0) {
+            sb.append(v);
+            return;
         }
+        sb.append("(");
+        int m = n/2;
+        logic(y, x, m);
+        logic(y, x + m, m);
+        logic(y + m, x, m);
+        logic(y + m, x + m, m);
+        sb.append(")");
     }
 }
---
+++ src/SSAFY/study/t1074/Main3.java
@@ -0,0 +1,35 @@
+package SSAFY.study.t1074;
+
+import java.util.Scanner;
+
+public class Main3 {
+    static int N, Y, X, CNT;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        Y = sc.nextInt();
+        X = sc.nextInt();
+        CNT = 0;
+        logic(0, 0, 1<<N);
+        System.out.println(CNT);
+        sc.close();
+    }
+
+    public static void logic(int y, int x, int n) {
+        if(n == 1) return;
+
+        n /= 2;
+        if (Y < y + n && X < x + n) {
+            logic(y, x, n);
+        } else if (Y < y + n && X >= x + n) {
+            CNT += n*n;
+            logic(y, x + n, n);
+        } else if (Y >= y + n && X < x + n) {
+            CNT += n*n*2;
+            logic(y + n, x, n);
+        } else {
+            CNT += n*n*3;
+            logic(y + n, x + n, n);
+        }
+    }
+}

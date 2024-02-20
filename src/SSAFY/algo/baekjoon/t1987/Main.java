d65fd6b1780e1926017581c0199c54c395429b1e
공원영 <dnjsdud9718@naver.com>
2024-02-20 10:40:30 +0900

    ssafy algo 1987 baekjoon

--- src/SSAFY/algo/baekjoon/t1987/Main.java
+++ src/SSAFY/algo/baekjoon/t1987/Main.java
@@ -3,8 +3,28 @@ package SSAFY.algo.baekjoon.t1987;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
+/*
+    백준 알파벳 1987
+    세로 R칸, 가로 C칸으로 된 표 모양의 보드가 있다. 보드의 각 칸에는 대문자 알파벳이 하나씩 적혀 있고, 좌측
+    상단 칸(1행 1열)에는 말이 놓여 있다.
+    말은 상하좌우 인접한 네 칸 중에 한 칸으로 이동할 수 있는데, 새로 이동한 칸에 적혀 있는 알파벳은 지금까지
+    지나온 모든 칸에 적혀 있는 알파벳과 달라야 한다. 즉, 같은 알파벳이 적힌 칸을 두 번 지날 수 없다.
 
+    좌측 상단에서 시작해서, 말이 최대한 몇 칸을 지날 수 있는지를 구하는 프로그램 작성.
+    말이 지나는 칸은 좌측 상단의 칸도 포함.
+
+    3 6
+    HFDFFB
+    AJHGDH
+    DGAGEH
+
+    숫자로 변환.
+    DFS 백트레킹.
+    BFS -> 안 된다.
+ */
 public class Main {
     static int R, C, ans;
     static int[][] src;

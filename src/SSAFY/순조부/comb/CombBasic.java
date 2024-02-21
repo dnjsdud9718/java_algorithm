8a9007ab252e73618ca4f4e4787bbb5112ab8738
공원영 <dnjsdud9718@naver.com>
2024-02-21 12:18:29 +0900

    study 2467 용액 lowerbounds

--- src/SSAFY/순조부/comb/CombBasic.java
+++ src/SSAFY/순조부/comb/CombBasic.java
@@ -2,8 +2,8 @@ package SSAFY.순조부.comb;
 
 // 조합
 public class CombBasic {
-    static int N = 5, K = 3;
-    static int[] src = new int[]{1,2,3,4,5};
+    static int N = 6, K = 2;
+    static int[] src = new int[]{-99, -2, -1, 1, 4, 98};
     static int[] tgt = new int[K];
     static int COUNT = 0;
     public static void main(String[] args) {
@@ -13,8 +13,12 @@ public class CombBasic {
     public static void comb(int srcIdx, int tgtIdx){
         if(tgtIdx == K){
             COUNT++;
-            for(int i=0; i<K; i++) System.out.print(tgt[i]+" ");
-            System.out.println();
+            int sum = 0;
+            for(int i=0; i<K; i++) {
+                sum += tgt[i];
+                System.out.print(tgt[i] + " ");
+            }
+            System.out.println(sum);
             return;
         }
         for(int i=srcIdx; i<N; i++){
